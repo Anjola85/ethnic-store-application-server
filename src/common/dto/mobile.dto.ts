cfa6ebd3198a7f72a87716a2efdd4147ff6aa138
anjola85 <anjola85@gmail.com>
2023-06-04 21:11:25 -0400

    fixed errors with signup

--- src/common/dto/mobile.dto.ts
+++ src/common/dto/mobile.dto.ts
@@ -1,18 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsPhoneNumber, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
 
 /**
  * Defining the structire for the mobile field
  */
 export class MobileDto {
   @ApiProperty({ example: '+14165555555' })
-  @IsNotEmpty()
+  @IsOptional()
   @IsPhoneNumber('CA')
   @IsString()
   phoneNumber: string;
 
   @ApiProperty({ example: '+1' })
-  @IsNotEmpty()
+  @IsOptional()
   @IsString()
   isoCode: string;
 
