07a7f9d040c4c1947dcd614cba3424bfd0586a0f
anjola85 <anjola85@gmail.com>
2023-11-06 11:23:53 -0500

    made modules global

--- src/providers/otp/twilio/twilio.module.ts
+++ src/providers/otp/twilio/twilio.module.ts
@@ -1,17 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
 import { ConfigService } from '@nestjs/config';
 import TwilioService from './twilio.service';
 
 @Module({
   imports: [
-    // MongooseModule.forFeature([
-    //   { name: User.name, schema: UserSchema },
-    //   { name: Customer.name, schema: CustomerSchema },
-    //   { name: Merchant.name, schema: MerchantSchema },
-    //   { name: UserAccount.name, schema: UserAccountSchema },
-    //   { name: Auth.name, schema: AuthSchema },
-    // ]),
     BullModule.registerQueue({
       name: 'twilioQueue',
       redis: {
